3c55d22077d07a196a74cf44b6fca8b4fe87eb23
Jeremy Cheng <81793294+Jeremy-Cheng-stack@users.noreply.github.com>
2022-04-11 21:40:02 -0400

    Update game.asm

--- game.asm
+++ game.asm
@@ -7,24 +7,28 @@
 # - Unit width in pixels: 4 (update this as needed)
 # - Unit height in pixels: 4 (update this as needed)
 # - Display width in pixels: 256 (update this as needed)
-# - Display height in pixels: 256 (update this as needed) # - Base Address for Display: 0x10008000 ($gp)
+# - Display height in pixels: 256 (update this as needed) 
+# - Base Address for Display: 0x10008000 ($gp)
 #
 # Which milestones have been reached in this submission?
-# (See the assignment handout for descriptions of the milestones) # - Milestone 1/2/3 (choose the one the applies)
+# - Milestone 3 
 #
 # Which approved features have been implemented for milestone 3?
-# (See the assignment handout for the list of additional features) # 1. (fill in the feature, if any)
-# 2. (fill in the feature, if any)
-# 3. (fill in the feature, if any)
-# ... (add more if necessary)
+# 1. Disappear Platforms  : The sky platform appears a disappears
+# 2. Win Conditions : The player collects 3 gold coins to win the game
+# 3. Lose Conditions : The player gets hit by a meteor or they fall into the lava
+# 4. Moving Objects  : Meteor falling from the sky
+# 5. Tracking Score : The yellow dots that appears bottom right after the user grabs a coin is how user can see score
+# 
 #
 # Link to video demonstration for final submission:
 # - (insert YouTube / MyMedia / other URL here). Make sure we can view it! #
 # Are you OK with us sharing the video with people outside course staff?
-# - yes / no / yes, and please share this project github link as well!
+# - yes 
 #
 # Any additional information that the TA needs to know:
-# - (write here, if any)
+# - Started a bit on pick up effects, but only created 1 out of 3 
+#
 # #####################################################################
 
 
